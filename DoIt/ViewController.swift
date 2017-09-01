4cd4d34850bb5b16c29ea6d578dd87eecd4710e1
Daniel E Chang <dchang8@us.ibm.com>
2017-09-01 11:35:36 -0400

    2/3 completed

--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -8,18 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tasks = makeTasks()
+        
+        tableView.dataSource = self //requires to add UITableViewDataSource class above
+        tableView.delegate = self   //requires to add UITableViewDelegate class above
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return 10 //number of rows
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        if task.important {
+            cell.textLabel?.text = " ‼️ \(task.name)"  //row content
+        } else {
+            cell.textLabel?.text = task.name  //row content
+        }
+        
+        return cell
+    }
+    
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Walk Casper"
+        task1.important = true
+        
+        let task2 = Task()
+        task2.name = "Sleep"
+        task2.important = false
+        
+        let task3 = Task()
+        task3.name = "Mow Lawn"
+        task3.important = true
+        
+        return [task1, task2, task3]
+        
+    }
 
+    @IBAction func plusTabbed(_ sender: Any) {
+        performSegue(withIdentifier: "AddSegue", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextVC =  segue.destination as! CreateTaskViewController
+        nextVC.previousVC = self
+    }
 
 }
 
